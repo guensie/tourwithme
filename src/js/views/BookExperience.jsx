2207b27f76a7b67e63431c44c77259ee8e7771b4
Donzalee Baker <jdonzalee@gmail.com>
2018-07-11 20:11:09 +0000

    Tourguide profile updated and booking experience added

---
+++ src/js/views/BookExperience.jsx
@@ -0,0 +1,91 @@
+import React from "react";
+import Flux from "@4geeksacademy/react-flux-dash";
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+import NavBar from '../components/NavBar';
+import { Link } from "react-router-dom";
+//include images into your bundle
+import ButtonComponent from '../components/ButtonComponent.jsx';
+// import ExploreTourGuides from '/exploretourguides.html';
+
+export class BookExperience extends React.Component {
+  render() {
+    return (
+        <div className="BookExperience">
+            <NavBar />
+            
+            <h1>Shopping Cart</h1>
+
+            <div class="shopping-cart">
+
+                <div class="column-labels">
+                    <label class="product-image">Image</label>
+                    <label class="product-details">Experience</label>
+                    <label class="product-price">Price</label>
+                    <label class="product-quantity">Quantity</label>
+                    <label class="product-removal">Remove</label>
+                    <label class="product-line-price">Total</label>
+                </div>
+
+                <div class="product">
+                    <div class="product-image">
+                        <img src="https://tse3.mm.bing.net/th?id=OIP.Ef0M_LMLN8gfhRuFSinJVwHaE8&pid=15.1&P=0&w=239&h=160"></img>
+                    </div>
+                    <div class="product-details">
+                        <div class="product-title">Surfing Experience</div>
+                        <p class="product-description">.</p>
+                    </div>
+                    <div class="product-price">15.00</div>
+                    <div class="product-quantity">
+                        <input type="number" value="1" min="1"></input>
+                    </div>
+                    <div class="product-removal">
+                        <button class="remove-product"> Remove </button>
+                    </div>
+                    <div class="product-line-price">15.00</div>
+                </div>
+
+                <div class="product">
+                    <div class="product-image">
+                        <img src="https://tse4.mm.bing.net/th?id=OIP.1qIrr18ZZUQdPvLPF1f2UgHaE8&pid=15.1&P=0&w=253&h=169"></img>
+                    </div>
+                    <div class="product-details">
+                        <div class="product-title">Surfing Experience</div>
+                    </div>
+                    <div class="product-price">30.00</div>
+                    <div class="product-quantity">
+                        <input type="number" value="1" min="1"></input>
+                    </div>
+                    <div class="product-removal">
+                        <button class="remove-product">Remove</button>
+                    </div>
+                </div>
+                <div class="total">
+                    <div class="total-item">
+                        <label>Subtotal</label>
+                        <div class="total-value" id="cart-subtotal">30.00</div>
+                    </div>
+                    <div class="total-item">
+                        <label>Tax (5%)</label>
+                        <div class="total-value" id="cart-tax">3.60</div>
+                    </div>
+                    <div class="total-item">
+                        <label>Shipping</label>
+                        <div class="total-value" id="cart-shipping">15.00</div>
+                    </div>
+                    <div class="total-item totals-item-total">
+                        <label>Grand Total</label>
+                        <div class="total-value" id="cart-total">63.60</div>
+                    </div>
+                </div>
+      
+                <button class="checkout">Checkout</button>
+
+            </div>
+        </div>
+      
+          );
+  }
+}
+
+export default BookExperience();
--- src/js/views/TourguideProfilePage.jsx
+++ src/js/views/TourguideProfilePage.jsx
@@ -49,43 +49,87 @@ export class TourguideProfilePage extends React.Component {
                     <li className="list-group-item"> <b> Upcoming Availability</b></li>
                 </ul>
             </div>
-                  <div className="month">      
-                    </div>
-                    <ul className="weekdays">
-                        <li>Mo</li>
-                        <li>Tu</li>
-                        <li>We</li>
-                        <li>Th</li>
-                        <li>Fr</li>
-                        <li>Sa</li>
-                        <li>Sun<li>
-                    <div className="month">      
-                    </div>
-                    <ul className="weekdays">
-                        <li>Mo</li>
-                        <li>Tu</li>
-                        <li>We</li>
-                        <li>Th</li>
-                        <li>Fr</li>
-                        <li>Sa</li>
-                        <li>Su</li>
-                    </ul>
-                    <ul className="days">  
-                        <li>1</li>
-                        <li>2</li>
-                        <li>3</li>
-                        <li>4</li>
-                        <li>5</li>
-                        <li>6</li>
-                        <li>7</li>
-                        <li>8</li>
-                        <li>9</li>
-                        <li><span className="active">10</span></li>
-                        <li>11</li>
-                        <li>12</li>
-                    </ul>
-           </div>
-        </div>  
+            <div class="container">
+                <div class="row">
+                    <div class="span12">
+    	               table class="table-condensed table-bordered table-striped">
+                <thead>
+                    <tr>
+                      <th colspan="7">
+                        <span class="btn-group">
+                            <a class="btn"><i class="icon-chevron-left"></i></a>
+                        	<a class="btn active">February 2012</a>
+                        	<a class="btn"><i class="icon-chevron-right"></i></a>
+                        </span>
+                      </th>
+                    </tr>
+                    <tr>
+                        <th>Su</th>
+                        <th>Mo</th>
+                        <th>Tu</th>
+                        <th>We</th>
+                        <th>Th</th>
+                        <th>Fr</th>
+                        <th>Sa</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td class="muted">29</td>
+                        <td class="muted">30</td>
+                        <td class="muted">31</td>
+                        <td>1</td>
+                        <td>2</td>
+                        <td>3</td>
+                        <td>4</td>
+                    </tr>
+                    <tr>
+                        <td>5</td>
+                        <td>6</td>
+                        <td>7</td>
+                        <td>8</td>
+                        <td>9</td>
+                        <td>10</td>
+                        <td>11</td>
+                    </tr>
+                    <tr>
+                        <td>12</td>
+                        <td>13</td>
+                        <td>14</td>
+                        <td>15</td>
+                        <td>16</td>
+                        <td>17</td>
+                        <td>18</td>
+                    </tr>
+                    <tr>
+                        <td>19</td>
+                        <td class="btn-primary"><strong>20</strong></td>
+                        <td>21</td>
+                        <td>22</td>
+                        <td>23</td>
+                        <td>24</td>
+                        <td>25</td>
+                    </tr>
+                    <tr>
+                        <td>26</td>
+                        <td>27</td>
+                        <td>28</td>
+                        <td>29</td>
+                        <td class="muted">1</td>
+                        <td class="muted">2</td>
+                        <td class="muted">3</td>
+                    </tr>
+                </tbody>
+            </table>
+            <input type="book" value="BOOK">
+        </div>
+	</div>
+</div>
+
+</div>
+          
+
+
       
          );
     }
